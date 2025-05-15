24eaf586c7656b7a21824ce73603d5a4eb91acc4
Reilly Baker <reillybaker9@gmail.com>
2025-05-14 20:42:25 -0400

    Initial commit

---
+++ src/main/java/net/reillyb/reillymod/ReillyMod.java
@@ -0,0 +1,16 @@
+package net.reillyb.reillymod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReillyMod implements ModInitializer {
+	public static final String MOD_ID = "reillymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/reillyb/reillymod/ReillyModClient.java
@@ -0,0 +1,10 @@
+package net.reillyb.reillymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReillyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
